31928421f9ba5df8276a426aa3396e4b75b936a5
Jonathan <senoriship@gmail.com>
2020-10-07 19:16:08 +0200

    added cookies (lol)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 var points = 0;
 
 function initiateSite(){
-    points = document.cookie;
+    points = Number(getCookie("score"));
+    document.getElementById("pointAmount").innerHTML = points + " Cookies";
 }
 
 function manualClick(clickValue){
     points = points + clickValue;
-    document.getElementById("pointAmount").innerHTML = points + " Cookies"
+    document.getElementById("pointAmount").innerHTML = points + " Cookies";
+    setCookie("score", points, 100);
 }
 
+function setCookie(cname, cvalue, exdays){
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires"+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
