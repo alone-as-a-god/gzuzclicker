eae605975248209aff5b23785865f5dc9a2acb5f
Jonathan Seyr <jonathan.seyr_br@snets.it>
2020-10-08 10:49:13 +0200

    added price display

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
 var points = 0;
 
 var upgrade = {
-    price:10,
+    price:25,
     exponent:1.3,
     amount:0,
-    id:"upgrade1display"
+    id:"upgrade1display",
+    priceId:"upgrade-price"
 };
 
 var clickUpgrade = {
-    price: 100,
+    price: 1000,
     exponent: 2.4,
     amount: 0,
-    id:"upgradeClickDisplay"
+    id:"upgradeClickDisplay",
+    priceId:"clickUpgrade-price"
 };
 
 function initiateSite(){
     points = Number(getCookie("score"));
     upgrade.amount = Number(getCookie("upgrade"));
     document.getElementById(upgrade.id).innerHTML = upgrade.amount;
+    upgrade.amount = Number(getCookie("clickUpgrade"));
+    document.getElementById(clickUpgrade.id).innerHTML = upgrade.amount;
     updateScore();
+}
+
+function calculatePrice(chosenUpgrade){
+    if(chosenUpgrade.amount == 0){                  //First purchase is always the base price of the ugprade
+        newPrice = Number(chosenUpgrade.price);
+    }else{
+        newPrice = Number(Number(chosenUpgrade.price) * Number(chosenUpgrade.exponent) **  (Number(chosenUpgrade.amount)));
+    }
+
+    newPrice = Math.round(newPrice * 10) / 10
+    //window.alert("New Price is: "+newPrice +  " of " + chosenUpgrade.id);
+    return newPrice;
+}
+
+function displayPrice(chosenUpgrade, newPrice){
+    //window.alert("New Price is: "+newPrice +  " of " + chosenUpgrade.id);
+    document.getElementById(chosenUpgrade.priceId).innerHTML = newPrice;
 }
 
 function updateScore(){
@@ -73,20 +94,16 @@ window.setInterval(function(){
 
 
 function purchaseUpgrade(chosenUpgrade){            //Purchase the given upgrade
-    if(chosenUpgrade.amount == 0){                  //First purchase is always the base price of the ugprade
-        newPrice = Number(chosenUpgrade.price);
-    }else{
-        newPrice = Number(Number(chosenUpgrade.price) * Number(chosenUpgrade.exponent) **  (Number(chosenUpgrade.amount)));
-    }
 
-    newPrice = Math.round(newPrice * 10) / 10
-
+    newPrice = calculatePrice(chosenUpgrade);
     if(points >= newPrice){
         points = points - newPrice;
         updateScore();
         chosenUpgrade.amount = chosenUpgrade.amount + 1;
     }
     document.getElementById(chosenUpgrade.id).innerHTML = chosenUpgrade.amount;
+    newPrice = calculatePrice(chosenUpgrade);
+    displayPrice(chosenUpgrade, newPrice);
 }
 
 
@@ -95,4 +112,17 @@ function purchaseUpgrade(chosenUpgrade){            //Purchase the given upgrade
 window.setInterval(function(){
     setCookie("score", points, 100);
     setCookie("upgrade", upgrade.amount, 100);
+    setCookie("clickUpgrade", clickUpgrade.amount, 100);
 }, 3000);
+
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
